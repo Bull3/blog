484aebff495002ecde247197715bb8dc80b96996
shenbb <1019067334@qq.com>
2019-11-26 18:02:02 +0800

    初始化

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    themeConfig: {
+        nav: [
+            { text: '首页', link: '/' },
+            { text: '路线', link: '/roadmap/' },
+        ],
+        displayAllHeaders: true,
+        sidebar: [
+            ['/','首页'],
+            ['/roadmap', '路线']
+        ]
+    }
+}
